1a316be1a722a89195990d18c08ea85b76adc574
Maly Phongsavanh and Nikki Wong <maly@>
2018-08-21 14:49:28 -0700

    add new transaction function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 // business logic
-function depositedBalance(name, deposit,initialDeposit) {
-  this.depositAmount = deposit;
+function Total(inputDeposit, initialDeposit, inputWithdrawal) {
+  this.depositAmount = inputDeposit;
   this.initialAmount = initialDeposit;
-  this.inputName = name;
-}
- depositedBalance.prototype.outcomeBalance = function() {
-   return this.inputName + "made a deposit of" + this.deposit;
- }
+  this.withdrawalAmount = inputWithdrawal;
 
+}
 var deposit = function(addingMoney) {
   if (inputDeposit > 0) {
     var total = initialDeposit + inputDeposit;
@@ -21,31 +18,48 @@ var deposit = function(addingMoney) {
 };
 
 // user interface logic
+function resetFields() {
+  $("input#nameInput").val("");
+  $("input#depositInput").val("");
+  $("input#depositAmount").val("");
+  $("input#withdrawalAmount").val("");
+}
+
+
 $(document).ready(function() {
   $("form.newAccount").submit(function(event) {
     event.preventDefault();
     $(".newBalance").show();
+    $(".depositAndWithdraw").show();
 
     var inputName = $("input#nameInput").val();
     initialDeposit = parseFloat($("input#depositInput").val());
 
     $(".balanceAmount").text(initialDeposit);
 
-    console.log(inputName, initialDeposit);
-
     $("form.depositAndWithdraw").submit(function(event) {
       event.preventDefault();
+      $(".newTransaction").show();
 
       inputDeposit = parseFloat($("input#depositAmount").val());
-      console.log(inputDeposit);
       inputWithdrawal = parseFloat($("input#withdrawalAmount").val());
 
+      // var newTotal = new Total(inputDeposit, initialDeposit, inputWithdrawal);
+
+
       var result = deposit(initialDeposit);
-      console.log(result);
 
       $(".balanceAmount").text(result);
-    })
 
+      $(".newTransaction").click(function(event) {
+        event.preventDefault();
+        resetFields()
+
+        initialDeposit = result;
+
+        $(".balanceAmount").text(result);
+      })
+    })
 
   });
 });
