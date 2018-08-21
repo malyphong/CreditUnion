aca4511ff09e0f7de35c6008200785f5f61d7999
Maly Phongsavanh and Nikki Wong <maly@>
2018-08-21 13:37:45 -0700

    add withdrawal and deposit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,45 @@
+// business logic
+var initialDeposit;
+var inputDeposit;
+var inputWithdrawal;
+
+var deposit = function(addingMoney) {
+  if (inputDeposit > 0) {
+    var total = initialDeposit + inputDeposit;
+
+    return total;
+  } else if (inputWithdrawal > 0) {
+    var total = initialDeposit - inputWithdrawal;
+
+    return total;
+  }
+};
+
+// user interface logic
 $(document).ready(function() {
   $("form.newAccount").submit(function(event) {
     event.preventDefault();
+    $(".newBalance").show();
 
     var inputName = $("input#nameInput").val();
-    var initialDeposit = parseInt($("input#depositInput").val());
+    initialDeposit = (parseFloat($("input#depositInput").val())).toFixed(2);
+
+    $(".balanceAmount").text(initialDeposit);
+
+    console.log(inputName, initialDeposit);
 
     $("form.depositAndWithdraw").submit(function(event) {
       event.preventDefault();
 
-      var inputDeposit = parseInt($("input#depositAmount").val());
-      var inputWithdrawal = parseInt($("input#withdrawalAmount").val());
-    })
-  });
+      inputDeposit = parseFloat($("input#depositAmount").val());
+      inputWithdrawal = parseFloat($("input#withdrawalAmount").val());
 
+      var result = deposit(initialDeposit);
+      console.log(result);
+
+      $(".balanceAmount").text(result);
+    })
+
+
+  });
 });
